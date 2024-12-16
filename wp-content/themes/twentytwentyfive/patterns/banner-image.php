232a05b3ed190b73546ae6e8a451c123c1f65be6
mnjo <100378645+MLB-78@users.noreply.github.com>
2024-12-16 15:59:44 +0100

    homapage done / just make footer

---
+++ wp-content/themes/twentytwentyfive/patterns/banner-image.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Title: Banner Image
+ * Slug: twentytwentyfive/banner-image
+ * Categories: featured
+ */
+?>
+
+<!-- wp:group {"className":"banner-section"} -->
+<div class="wp-block-group banner-section">
+    <!-- wp:image {"sizeSlug":"full","className":"banner-image"} -->
+    <figure class="wp-block-image size-full banner-image">
+        <img src="<?php echo esc_url(get_theme_file_uri('assets/images/bannieres.webp')); ?>" alt="Bannière pneus 4 saisons"/>
+    </figure>
+    <!-- /wp:image -->
+</div>
+<!-- /wp:group --> 
